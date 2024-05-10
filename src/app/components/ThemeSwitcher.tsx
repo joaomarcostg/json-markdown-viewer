c4ef50df87e4666dbe99e591caa257fae1756a97
Joao Marcos Torres Gardingo <joaomarcos_tg@outlook.com>
2024-05-10 15:55:02 -0300

    Create ThemeProvider and Toggle component

---
+++ src/app/components/ThemeSwitcher.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useTheme } from "next-themes";
+
+export default function ThemeSwitcher() {
+  const { setTheme, resolvedTheme } = useTheme();
+
+  const toggleTheme = () => {
+    const newTheme = resolvedTheme === "light" ? "dark" : "light";
+
+    setTheme(newTheme);
+  };
+
+  return (
+    <button
+      className=" text-white cursor-pointer outline-none border-blue-900 dark:border-blue-200 border-solid border-2 bg-blue-600 px-2 py-1 rounded-md text-sm font-medium hover:bg-blue-500 mb-4"
+      onClick={toggleTheme}
+    >
+      Current Theme: {resolvedTheme}
+    </button>
+  );
+}
